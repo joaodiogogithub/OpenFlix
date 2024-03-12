c0456ed76a30eccbfe152c1275d39ec14522e43f
joaodigogithub <joaodigoemail@gmail.com>
2024-03-12 09:22:45 -0300

    OpenFLIX (DEMO)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OpenFlix
+![alt text](image.png)
+![alt text](image-1.png)
---
+++ script.js
@@ -0,0 +1,9 @@
+function playVideo() {
+    var video = document.querySelector("video");
+    video.play();
+}
+
+function pauseVideo() {
+    var video = document.querySelector("video");
+    video.pause();
+}
