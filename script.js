65c1357f830eb5b3b914a7ae6b5c3f7b4e03c1f9
joaodigogithub <joaodigoemail@gmail.com>
2024-03-12 13:50:32 -0300

    Feat: Add Dashboard screen and sidebar

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function pauseVideo() {
     var video = document.querySelector("video");
     video.pause();
 }
+
+function toggleSidebar() {
+    var sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("active");
+
+    if (sidebar.classList.contains("active")) {
+        botao.innerHTML = "X";
+    }
+    else {
+        botao.innerHTML ="☰";
+    }
+}
